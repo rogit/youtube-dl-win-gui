d44732fd884e2927d7f7b246275616265298b9e8
rogit <rogit@rogit>
2020-02-06 18:52:21 +0300

    First commit

---
+++ www/api/index.php
@@ -0,0 +1,388 @@
+<?php
+
+define ( 'STAGE_GET_TITLE', 'getTitle' );
+define ( 'STAGE_MAIN', 'main' );
+
+function ret400 () {
+	header ( "HTTP/1.1 400 Bad Request", true, 400 );
+	die ();
+}
+
+function openDb () {
+	$datadir = dirname ( $_SERVER['SCRIPT_FILENAME'] ) . '\\..\\..\\data\\';
+	if ( !file_exists ( $datadir . 'data.db3' )) copy ( $datadir . '_data.db3', $datadir . 'data.db3' );
+	return new SQLite3 ( $datadir . 'data.db3' );
+}
+
+function getHTTPResponseCode ( $http_response_header ) {
+	foreach ( $http_response_header as $header ) {
+		if ( preg_match ( "/^http.* ([0-9]+)/i", $header, $matches )) {
+			return intval ( $matches[1] );
+		}
+	}
+	return false;
+}
+
+function startJob ( $jobId, $jobCmd ) {
+	$options = array (
+		'http' => array (
+			'header' => "Content-type: application/x-www-form-urlencoded",
+			'method' => 'POST',
+			'content' => http_build_query ( array ( 'jobId' => $jobId, 'jobCmd' => $jobCmd, 'secret' => get_cfg_var ('SECRET_INTER' ) ))
+		)
+	);
+	$context = stream_context_create ( $options );
+	file_get_contents ( 'http://127.0.0.1:' . get_cfg_var ('PORT_INTER' ), false, $context );
+	return getHTTPResponseCode ( $http_response_header ) === 200;
+}
+
+function addTaskToDB ( $db, $link, $settings ) {
+	if ( linkExist ( $db, $link )) return true;
+	$video = $settings['video'];
+	$youtubeDlExe = convertToLocale ( $settings['youtubeDlExe'] ) . "\\youtube-dl.exe";
+	$id = "i" . time () . mt_rand ( 100000, 999999 );
+	$jobCmd = "$youtubeDlExe --encoding utf-8 --no-check-certificate --no-color";
+	if ( $settings['proxy'] !== '' ) $jobCmd .= ' --proxy ' . $settings['proxy'];
+	$jobCmd .= ' --get-title ' . $link;
+	$finished = $exitCode = 0;
+	if ( !startJob ( $id, $jobCmd )) {
+		$finished = $exitCode = 1;
+	}
+	$stmt = $db->prepare ( "INSERT INTO tasks ( id, link, started, finished, exitCode, addTime, stage, video, downloadProgress, fileSize, title, showTitle ) VALUES ( '$id', :link, 0, $finished, $exitCode, " . time() . ", '" . STAGE_GET_TITLE . "', $video, 0.0, '', '', 0 )" );
+	$stmt->bindParam ( ':link', $link );
+	$stmt->execute();
+	$stmt->close ();
+}
+
+function deleteTask ( $db, $id ) {
+	$stmt = $db->prepare ( "delete from tasks where id = :id" );
+	$stmt->bindParam ( ':id', $id );
+	$stmt->execute();
+	$stmt = $db->prepare ( "delete from output where taskId = :id" );
+	$stmt->bindParam ( ':id', $id );
+	$stmt->execute();
+	$stmt->close ();
+}
+
+function deleteAllTasks ( $db ) {
+	$db->query ( "delete from tasks" );
+	$db->query ( "delete from output" );
+}
+
+function getTasks ( $db ) {
+	$tasks = [];
+	$result = $db->query ( "select * from tasks order by addTime desc" );
+	while ( $row = $result->fetchArray ( SQLITE3_ASSOC )) $tasks[] = $row;
+	return $tasks;
+}
+
+function linkExist ( $db, $link ) {
+	$stmt = $db->prepare ( "select id from tasks where link = :link and finished = 0" );
+	$stmt->bindParam ( ':link', $link );
+	$result = $stmt->execute();
+	$ret = $result->fetchArray ( SQLITE3_NUM ) !== false;
+	$stmt->close ();
+	return $ret;
+}
+
+function getTask ($db, $id ) {
+	$stmt = $db->prepare ( "select * from tasks where id = :id" );
+	$stmt->bindParam ( ':id', $id );
+	$result = $stmt->execute();
+	$ret = $result->fetchArray ( SQLITE3_ASSOC );
+	$stmt->close ();
+	return $ret;
+}
+
+function extractDownloadSizeAndProgress ( $outText, $downloadProgress, $fileSize ) {
+	if ( preg_match ( "/^\\[download\\]\s+([0-9\\.]+)%\s+of\s+(\S+)($|\s+)/", $outText, $matches )) return array (  $matches[1], $matches[2] );
+	else return array ( $downloadProgress, $fileSize );
+}
+
+function extractTitle ( $outText, $task ) {
+	if (( $task['stage'] == STAGE_GET_TITLE ) && ( $outText !== '' )) return $outText;
+	else return $task['title'];
+}
+
+function updateTask ( $db, $data, $task ) {
+	$downloadProgress = $task['downloadProgress'];
+	$fileSize = $task['fileSize'];
+	$title = $task['title'];
+	if ( count ( $data['output'] )) {
+		$stmt = $db->prepare ( "INSERT INTO output ( taskId, outText ) values ( :id, :out )" );
+		$stmt->bindParam ( ':id', $data['id'] );
+		foreach ( $data['output'] as $out ) {
+			$outText = trim ( hex2bin ( $out ));
+			list ( $downloadProgress, $fileSize ) = extractDownloadSizeAndProgress ( $outText, $task['downloadProgress'], $task['fileSize'] );
+			$title = extractTitle ( $outText, $task );
+			$stmt->bindParam ( ':out', $outText );
+			$stmt->execute ();
+		}
+		$stmt->close ();
+	}
+	$finished = intval ( $data['finished'] );
+	$exitCode = intval ( $data['exitCode'] );
+	$showTitle = $task['showTitle'];
+	$stage = $task['stage'];
+	if (( $stage == STAGE_GET_TITLE ) && ( $finished )) {
+		if ( $exitCode ) {
+			$title = '';
+		} else {
+			$finished = 0;
+			$stage = STAGE_MAIN;
+			$showTitle = 1;
+			$settings = getSettings ( $db );
+			$youtubeDlExe = convertToLocale ( $settings['youtubeDlExe'] ) . "\\youtube-dl.exe";
+			$jobCmd = "$youtubeDlExe --encoding utf-8 --no-check-certificate --no-color";
+			if ( $settings['proxy'] !== '' ) $jobCmd .= ' --proxy ' . $settings['proxy'];
+			$targetFolder = convertToLocale ( $settings['targetFolder'] );
+			$jobCmd .= ' -v --newline';
+			if ( $task['video'] ) {
+				$jobCmd .= ' -o "' . $targetFolder . '\%(title)s.%(ext)s" ' . $task['link'];
+			} else {
+				$ffmpegHome = convertToLocale ( $settings['ffmpegHome'] );
+				$jobCmd .= ' --extract-audio --audio-format mp3 --ffmpeg-location "' . $ffmpegHome . '" -o "' . $targetFolder . '\%(title)s.%(ext)s" ' . $task['link'];
+			}
+			if ( !startJob ( $task['id'], $jobCmd )) {
+				$finished = $exitCode = 1;
+			}
+		}
+	}
+	$stmt = $db->prepare ( "UPDATE tasks set title = :title, showTitle = $showTitle, stage = '$stage', downloadProgress = $downloadProgress, fileSize = :fileSize, started = " . intval ( $data['started'] ) . ", finished = $finished, exitCode = $exitCode where id = :id" );
+	$stmt->bindParam ( ':title', $title );
+	$stmt->bindParam ( ':id', $data['id'] );
+	$stmt->bindParam ( ':fileSize', $fileSize );
+	$stmt->execute ();
+	$stmt->close ();
+}
+
+function getLog ( $db, $taskId ) {
+	$output = [];
+	$result = $db->query ( "select outText from output where taskId = '$taskId' order by id" );
+	while ( $row = $result->fetchArray ( SQLITE3_NUM )) $output[] = $row[0];
+	return $output;
+}
+
+function findYoutubeDlExe ( $dir ) {
+	$files = scandir ( $dir );
+	foreach ( $files as $file ) {
+		if ( $file == 'youtube-dl.exe' ) return "$dir";
+		if ( $file == '.' ) continue;
+		if ( $file == '..' ) continue;
+		if ( is_dir ( "$dir/$file" )) {
+			$path = findYoutubeDlExe ( "$dir/$file" );
+			if ( $path !== false ) return $path;
+		}
+	}
+	return false;
+}
+
+function getSettings ( $db ) {
+	$settings = [];
+	$result = $db->query ( "select name, value from settings" );
+	while ( $row = $result->fetchArray ( SQLITE3_ASSOC )) $settings[$row['name']] = $row['value'];
+	if ( !isset ( $settings['youtubeDlExe'] )) {
+		$path = findYoutubeDlExe ( getcwd() . "../../../" );
+		if ( $path !== false ) {
+			$settings['youtubeDlExe'] = realpath ( $path );
+			$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'youtubeDlExe', :value )" );
+			$stmt->bindParam ( ':value', $settings['youtubeDlExe'] );
+			$stmt->execute();
+			$stmt->close ();
+		} else $settings['youtubeDlExe'] = '';
+	}
+	if ( !isset ( $settings['targetFolder'] )) {
+		$settings['targetFolder'] = realpath (  "../../" );
+		$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'targetFolder', :value )" );
+		$stmt->bindParam ( ':value', $settings['targetFolder'] );
+		$stmt->execute();
+		$stmt->close ();
+	}
+	if ( !isset ( $settings['video'] )) {
+		$settings['video'] = 1;
+		$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'video', 1 )" );
+		$stmt->execute();
+		$stmt->close ();
+	}
+	if ( !isset ( $settings['ffmpegHome'] )) {
+		$settings['ffmpegHome'] = '';
+		$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'ffmpegHome', '' )" );
+		$stmt->execute();
+		$stmt->close ();
+	}
+	if ( !isset ( $settings['proxy'] )) {
+		$settings['proxy'] = '';
+		$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'proxy', '' )" );
+		$stmt->execute();
+		$stmt->close ();
+	}
+	if ( !isset ( $settings['locale'] )) {
+		$settings['locale'] = 'en-US';
+		$stmt = $db->prepare ( "INSERT INTO settings ( name, value ) values ( 'locale', 'en-US' )" );
+		$stmt->execute();
+		$stmt->close ();
+	}
+	return $settings;
+}
+
+function saveSettings ( $db, $settings ) {
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'youtubeDlExe'" );
+	$stmt->bindParam ( ':value', trim ( $settings['youtubeDlExe'] ));
+	$stmt->execute();
+	$stmt->close ();
+	
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'targetFolder'" );
+	$stmt->bindParam ( ':value', trim ( $settings['targetFolder'] ));
+	$stmt->execute();
+	$stmt->close ();
+	
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'video'" );
+	$stmt->bindParam ( ':value', $settings['video'] );
+	$stmt->execute();
+	$stmt->close ();
+	
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'ffmpegHome'" );
+	$stmt->bindParam ( ':value', trim ( $settings['ffmpegHome'] ));
+	$stmt->execute();
+	$stmt->close ();
+	
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'proxy'" );
+	$stmt->bindParam ( ':value', trim ( $settings['proxy'] ));
+	$stmt->execute();
+	$stmt->close ();
+	
+	$stmt = $db->prepare ( "UPDATE settings set value = :value where name = 'locale'" );
+	$stmt->bindParam ( ':value', $settings['locale'] );
+	$stmt->execute();
+	$stmt->close ();
+}
+
+function convertToLocale ( $text ) {
+	if ( preg_match ( "/\\D(\\d+)$/", setlocale ( LC_CTYPE, 0 ), $matches )) {
+		$cp = "cp" . $matches[1];
+		$ret = iconv ( 'utf-8', $cp, $text );
+		if ( $ret === false ) return $text;
+		else return $ret;
+	} else return $text;
+}
+
+header ( "Content-Type: text/html; charset=utf-8" );
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+#error_log ( print_r ( $_POST , 1 ));
+	if ( !isset ( $_POST['data'] )) ret400 ();
+	$data = json_decode ( $_POST['data'], true, 3, JSON_OBJECT_AS_ARRAY | JSON_BIGINT_AS_STRING );
+	if ( !is_array ( $data )) ret400 ();
+}
+
+$ret = [];
+$db = openDb ();
+switch ( $_SERVER['PATH_INFO'] ) {
+	case "/getTasks":
+		$ret['tasks'] = getTasks ( $db );
+	break;
+	
+	case "/getSettings":
+		$ret = getSettings ( $db );
+	break;
+	
+	case "/checkSaveSettings":
+		$ret['error'] = 0;
+		$ret['val'] = '';
+		if ( !file_exists ( $data['settings']['youtubeDlExe'] )) {
+			$ret['val'] = $data['settings']['youtubeDlExe'];
+			$ret['error'] = 1;
+		} else {
+			if ( !is_dir ( $data['settings']['youtubeDlExe'] )) {
+				$ret['val'] = $data['settings']['youtubeDlExe'];
+				$ret['error'] = 3;
+			} else {
+				if ( !is_executable ( $data['settings']['youtubeDlExe'] . "\\youtube-dl.exe" ) ) {
+					$ret['val'] = $data['settings']['youtubeDlExe'];
+					$ret['error'] = 2;
+				}
+			}
+		}
+		if ( $data['settings']['targetFolder'] === '' ) {
+			$ret['error'] = 4;
+		} else {
+			if ( !file_exists ( $data['settings']['targetFolder'] ) ) {
+				$ret['val'] = $data['settings']['targetFolder'];
+				$ret['error'] = 1;
+			} else {
+				if ( !is_dir ( $data['settings']['targetFolder'] )) {
+					$ret['val'] = $data['settings']['targetFolder'];
+					$ret['error'] = 3;
+				}
+			}
+		}
+		if ( !$data['settings']['video'] ) {
+			if ( $data['settings']['ffmpegHome'] === '' ) {
+				$ret['error'] = 4;
+			} else {
+				if ( !is_dir ( $data['settings']['ffmpegHome'] ) ) {
+					$ret['val'] = $data['settings']['ffmpegHome'];
+					$ret['error'] = 3;
+				}
+			}
+		}
+		if ( !$ret['error'] ) saveSettings ( $db, $data['settings'] );
+	break;
+	
+	case "/openTargetFolder":
+		$settings = getSettings ( $db );
+		if ( is_dir ( $settings['targetFolder'] )) shell_exec ( "explorer.exe \"" . $settings['targetFolder'] . "\"" );
+	break;
+
+	case "/newTask":
+		if ( preg_match_all ( "/(\S+)/" , $data['text'], $matches )) {
+			$settings = getSettings ( $db );
+			foreach ( $matches[0] as $link ) if ( preg_match ( "/^http/i", $link )) addTaskToDB ( $db, $link, $settings );
+		}
+		$ret['tasks'] = getTasks ( $db );
+	break;
+	
+	case "/deleteTask":
+		if ( getTask ( $db, $data['id'] === false )) die ();
+		deleteTask ( $db, $data['id'] );
+	break;
+	
+	case "/deleteAllTasks":
+		deleteAllTasks ( $db );
+	break;
+
+	case "/putJob":
+		$task = getTask ( $db, $data['id'] );
+		if ( $task === false ) die ( "terminate" );
+		updateTask ( $db, $data, $task );
+	break;
+	
+	case "/getLog":
+		if ( getTask ( $db, $data['id'] === false )) die ();
+		$ret['output'] = getLog ( $db, $data['id'] );
+	break;
+	
+	case "/getYoutubeDlVersion":
+		$ret['version'] = '';
+		$youtubeDlExe = convertToLocale ( $data['youtubeDlExe'] ) . "\\youtube-dl.exe";
+		if ( is_executable ( $youtubeDlExe )) {
+			exec ( "\"$youtubeDlExe\" --version", $out );
+			$ret['version'] = $out[0];
+		}
+	break;
+	
+	case "/updateYoutubeDl":
+		$OPTIONS = '" --update';
+		if ( $data['proxy'] !== '' ) $OPTIONS .= ' --proxy ' . $data['proxy'];
+		exec ( '"' . $data['youtubeDlExe'] . $OPTIONS );
+	break;
+	
+	case "/dummy":
+	break;
+	
+	default:
+		ret400 ();
+	break;
+}
+$db->close();
+echo json_encode ( $ret, JSON_NUMERIC_CHECK );
+?>
